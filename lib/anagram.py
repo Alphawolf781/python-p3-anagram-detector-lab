70038130f47c7b9d5c05ef6596ba889a49865d78
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-08-30 13:12:19 +0300

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        # Store the sorted version of the word
+        self.sorted_word = ''.join(sorted(word))
+    
+    def match(self, possible_anagrams):
+        # Find all matches by comparing sorted versions
+        return [anagram for anagram in possible_anagrams if ''.join(sorted(anagram)) == self.sorted_word]
